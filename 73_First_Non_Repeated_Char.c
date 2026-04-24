da180a48c50861dc5594bc81666816b5dfe6d526
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2026-04-24 21:51:46 +0530

    Day 73

---
+++ 73_First_Non_Repeated_Char.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+
+    printf("Enter the string: ");
+    scanf("%s", s);
+
+    int freq[26] = {0};
+
+    // Step 1: Count frequency
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Step 2: Find first non-repeating character
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (freq[s[i] - 'a'] == 1) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+    }
+
+    // If all characters repeat
+    printf("$\n");
+
+    return 0;
+}
