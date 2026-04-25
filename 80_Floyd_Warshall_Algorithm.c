f3314c15e307be53aa5ee50e7dc59a30da28c4a0
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2026-04-25 22:37:55 +0530

    Day 80

---
+++ 80_Floyd_Warshall_Algorithm.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define MAX 100
+#define INF 99999
+
+int main() {
+    int n;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    int dist[MAX][MAX];
+
+    printf("Enter adjacency matrix (use -1 for no edge):\n");
+
+    // Input and initialization
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+
+            if (dist[i][j] == -1 && i != j)
+                dist[i][j] = INF;
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Output result
+    printf("Shortest distance matrix:\n");
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (dist[i][j] == INF)
+                printf("-1 ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
