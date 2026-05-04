cf395ab4835c16f10343273e8641bad1c8d3fc29
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2026-05-04 09:05:02 +0530

    Day 92

---
+++ 92_Quick_Sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// Lomuto partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];   // choose last element as pivot
+    int i = low - 1;         // index of smaller element
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            // swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // place pivot at correct position
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;  // return pivot index
+}
+
+// Quick Sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);   // left side
+        quickSort(arr, pi + 1, high);  // right side
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
